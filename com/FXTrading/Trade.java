65152fc69831d3d2b418c6ad1c48581682d250c8
Suraj <suravanshisuraj37@gmail.com>
2023-08-26 18:22:14 +0530

    FX Trading Application

---
+++ com/FXTrading/Trade.java
@@ -0,0 +1,39 @@
+package com.FXTrading;
+
+public class Trade {
+    private static int tradeCounter = 1;
+
+    private int tradeNo;
+    private String currencyPair;
+    private String customerName;
+    private double amount;
+    private double rate;
+
+    public Trade(String currencyPair, String customerName, double amount, double rate) {
+        this.tradeNo = tradeCounter++;
+        this.currencyPair = currencyPair;
+        this.customerName = customerName;
+        this.amount = amount;
+        this.rate = rate;
+    }
+
+    public int getTradeNo() {
+        return tradeNo;
+    }
+
+    public String getCurrencyPair() {
+        return currencyPair;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+}
